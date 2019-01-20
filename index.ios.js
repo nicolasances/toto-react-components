c3092a91920b24ae86cd713d7a654a52ebf2d352
nicolas <nicolas.matteazzi@gmail.com>
2019-01-20 10:00:41 +0100

    Added export of Toto Icon Button

--- index.ios.js
+++ index.ios.js
@@ -3,11 +3,13 @@ var TotoSignIn = require('./login/TotoSignIn');
 var TotoEventBus = require('./event/TotoEventBus');
 var TotoTheme = require('./theme/ThemeColors');
 var TotoTitleBar = require('./title/TotoTitleBar');
+var TotoIconButton = require('./title/TotoIconButton');
 
 module.exports = {
   TotoLoginComponent: TotoLoginComponent.TotoLoginComponent,
   TotoSignIn: TotoSignIn.TotoSignIn,
   TotoEventBus: TotoEventBus,
   TotoTheme: TotoTheme.default,
-  TotoTitleBar: TotoTitleBar.default
+  TotoTitleBar: TotoTitleBar.default,
+  TotoIconButton: TotoIconButton.default
 }
